d159cb6602c16beb47c2651516fa3b0285c564b5
DenysMakarov <mdamywork@gmail.com>
2022-04-18 15:47:11 +0300

    add component - Status in Loan page

--- src/App.js
+++ src/App.js
@@ -11,37 +11,11 @@ import Activities from "./cmps/MockPageForRouter/Activities";
 import Statics from "./cmps/MockPageForRouter/Statics";
 import Leads from "./cmps/MockPageForRouter/Leads";
 
-const headerMock1 = {
-    headerTitle: '291 North 8 street, Brooklyn, NY',
-    reqNo: 'Request no. 203-12209',
-    owner: 'Owner: Amir Cohen',
-    update: 'Last update: 22/09/22, 18:30'
-}
-const detailMock1 = [
-    {entry: 'Amount', value: '$970,000'},
-    {entry: 'Purpose', value: 'Purchase'},
-    {entry: 'R.C Date', value: '05.09.22'},
-    {entry: 'Duration', value: '12 months'},
-    {entry: 'Repayment', value: 'Refinance'},
-    {entry: 'Rate', value: '11%'},
-    {entry: 'Purchase Price', value: '$630,000'}]
-
-const detailMock2 = [
-    {entry: 'Phone', value: '646-3055952'},
-    {entry: 'Email', value: 'abraham@real.com'},
-    {entry: 'Date Of Birth', value: '02/02/1981'},
-    {entry: 'Credit Score', value: '690'}]
-
-const borrowerMock = 'Abraham Gabrielovich'
 
 function App() {
     return (
         <div className="App">
             <MainNavigation/>
-
-            {/*
-                  Mock routes from Denys
-            */}
             <Routes>
                 {
                     ['/', 'dashboard'].map(path => <Route
@@ -50,29 +24,12 @@ function App() {
                         element={<Dashboard/>}
                     />)
                 }
-                <Route path='/requests' element={<Requests/>}/>
+                {/*<Route path='/requests' element={<Requests/>}/>*/}
+                <Route path='/requests' element={<LoanPage/>}/>
                 <Route path='/activities' element={<Activities/>}/>
                 <Route path='/statics' element={<Statics/>}/>
                 <Route path='/leads' element={<Leads/>}/>
             </Routes>
-
-
-
-
-            <CardComp
-                headerDetails={headerMock1}
-                cardDetails={detailMock1}
-            />
-            <br/>
-            <CardComp
-                cardDetails={detailMock2}
-                borrower={borrowerMock}
-            />
-
-            {/*  <CardComp
-        cardDetails={detailMock2}
-      /> */}
-            {/* <LoanPage /> */}
 
         </div>
     );
